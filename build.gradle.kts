cda2dc89cb16d3c2a329e1f01da27c77833baaad
SreedharGovind <140377328+SreedharGovind@users.noreply.github.com>
2023-10-11 12:44:03 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("pl.oakfusion:chromedriver:2.9")
+    testImplementation("org.junit:junit-bom:5.9.1")
+    implementation("org.seleniumhq.selenium:selenium-java:3.141.59")
+    implementation("org.seleniumhq.selenium:selenium-java:4.12.0")
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
