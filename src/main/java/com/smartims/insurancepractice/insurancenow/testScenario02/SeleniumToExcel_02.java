0af2363a02f98981c5068414870d1aa3e02b21d7
shazzameter <munnablastz1999@gmail.com>
2023-10-30 17:25:44 +0530

    counter class and updated sheet of excel

--- src/main/java/com/smartims/insurancepractice/insurancenow/testScenario02/SeleniumToExcel_02.java
+++ src/main/java/com/smartims/insurancepractice/insurancenow/testScenario02/SeleniumToExcel_02.java
@@ -6,12 +6,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 public class SeleniumToExcel_02 {
+
+
+    private static int currentColumnIndex;
+    private static final String counterFilePath = "counter.properties";
+
+    private static void incrementColumnIndex() {
+        currentColumnIndex++;
+        saveCounterToFile();
+    }
+
+    public static int getCurrentColumnIndex() {
+        return currentColumnIndex;
+    }
+
+    public static void setCurrentColumnIndex(int newIndex) {
+        currentColumnIndex = newIndex;
+        saveCounterToFile();
+    }
+
+    public static String getCounterFilePath() {
+        return counterFilePath;
+    }
+
+    private static void saveCounterToFile() {
+        Properties properties = new Properties();
+        properties.setProperty("currentColumnIndex", String.valueOf(currentColumnIndex));
+
+        try (OutputStream outputStream = new FileOutputStream(counterFilePath)) {
+            properties.store(outputStream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initializeCurrentColumnIndex() {
+        Properties properties = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream(counterFilePath)) {
+            properties.load(fileInputStream);
+            String currentIndexValue = properties.getProperty("currentColumnIndex");
+            if (currentIndexValue != null) {
+                currentColumnIndex = Integer.parseInt(currentIndexValue);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     WebElement premiumValue(ChromeDriver driver, String transaction) {
 
@@ -37,10 +82,13 @@ public class SeleniumToExcel_02 {
     }
 
     public void premium(ChromeDriver driver, String transaction) throws IOException {
+        initializeCurrentColumnIndex(); // Initialize the currentColumnIndex from the properties file
+        System.out.println("currentColumnIndex at the beginning: " + currentColumnIndex);
 
         FileInputStream fis = new FileInputStream(new File("INOW Automation Data Sheet.xlsx"));
         Workbook workbook = new XSSFWorkbook(fis);
-        Sheet sheet = workbook.getSheet("PolicyNumber&Premium");
+        String SheetName = "PremiumValues";
+        Sheet sheet = workbook.getSheet(SheetName);
         WebElement data = this.premiumValue(driver, transaction);
         String capturedData = data.getText();
         for (Row row : sheet) {
@@ -48,19 +96,21 @@ public class SeleniumToExcel_02 {
                 if (cell != null && cell.getCellType() == CellType.STRING
                         && cell.getStringCellValue().equals(transaction)) {
                     int columnIndex = cell.getColumnIndex();
-                    Cell dataCell = row.createCell(columnIndex + 1);
+                    Cell dataCell = row.createCell(columnIndex + currentColumnIndex);
                     dataCell.setCellValue(capturedData);
                     System.out.println(capturedData);
-
                     break;
                 }
             }
         }
-
         FileOutputStream fos = new FileOutputStream(new File("INOW Automation Data Sheet.xlsx"));
         workbook.write(fos);
         fos.close();
-
     }
 
+    // Add this method to increment the counter after all transactions finish execution
+    public static void incrementCounterAfterAllTransactions() {
+        incrementColumnIndex();
+        System.out.println("currentColumnIndex after increment: " + currentColumnIndex);
+    }
 }
